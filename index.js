42bc7e3c7113a4b6b7c315cfe34d8f55406976c2
Cédric David <cedric.david@btopenworld.com>
2018-07-17 00:10:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function sumPrimes(num) {
   let sum = 0;
 
   function primeCheck(i) {
-    for (let j = 2; j<i; j++){
+    for (let j = 2; j < i; j++){
       if(i % j === 0){
         return false;
       }    
     }
     return true;
   }
-  
-  for(let i =2; i<=num;i++){
+
+  for(let i = 2; i <= num; i++){
     if (primeCheck(i)){
       sum +=i;
     }
